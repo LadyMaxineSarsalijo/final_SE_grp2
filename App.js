965c1a36ee6e14649198ce4d47550ef60c6323f1
subibiclientjohn <alexvgamer2@gmail.com>
2023-01-30 14:51:57 +0800

    registration

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import GetDone from './authentication/GetDone';
+import Registration from './authentication/registration';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="GetDone" screenOptions={{headerShown:false}}>
                 <Stack.Screen name="GetDone" component={GetDone} />
+                <Stack.Screen name="Registration" component={Registration} />
 
 
       </Stack.Navigator>
